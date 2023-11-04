815b2f467d6b9a995cf0e75adbef8c59ee13d9c5
Bence 'Bensz' Tóth <48518363+Bensz86@users.noreply.github.com>
2023-11-04 08:43:25 +0100

    update

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config['STATIC_FOLDER'] = 'static'
 socketio = SocketIO(app) 
 
 @app.route('/')
@@ -15,7 +17,8 @@ def admin():
 
 @app.route('/controller')
 def controller():
-    user = request.args.get('user') 
+    user = request.args.get('user')
+    print(f"User {user} connected")
     return render_template('controller.html')
 
 @socketio.on('message_from_controller')
@@ -23,6 +26,39 @@ def handle_message(message):
     print("Received message from controller:", message)
     socketio.emit('message_to_default', message)
     socketio.emit('custom_message_to_controller', 'Message received by the server!')
+
+@socketio.on('message_from_admin')
+def handle_message(message):
+    print("Received message from admin:", message)
+    #socketio.emit('message_to_default', message)
+    socketio.emit('custom_message_to_controller', message)
+
+@socketio.on('ping')
+def handle_message(message):
+    print("User "+ str(request.args.get('user')) + " alive:", message)
+    #socketio.emit('message_to_default', message)
+
+def get_question_data(index, filename='static/data/config.json', delimiter='|'):
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            questions = data.get('questions')
+            if questions and 0 <= index < len(questions):
+                question = questions[index]['question']
+                options = '|'.join(questions[index]['options'])
+                correct_answer = questions[index]['correctAnswer']
+                return f"{question}{delimiter}{options}{delimiter}{correct_answer}"
+            else:
+                return "Invalid index"
+    except FileNotFoundError:
+        return "File not found"
+    except json.JSONDecodeError:
+        return "Invalid JSON format"
+    
+# Example usage
+index = 0  # Change this index to get data for a different question
+question_data = get_question_data(index)
+#print(question_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
